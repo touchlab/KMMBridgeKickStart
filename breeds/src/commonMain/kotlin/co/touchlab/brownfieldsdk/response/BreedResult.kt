5ee8033189bea229bfd76f7b10028cf5a51ecaf3
Julia Jakubcova <j.jakubcova@touchlab.co>
2022-10-19 16:25:33 +0200

    Remove internal from BreedResult.

--- breeds/src/commonMain/kotlin/co/touchlab/brownfieldsdk/response/BreedResult.kt
+++ breeds/src/commonMain/kotlin/co/touchlab/brownfieldsdk/response/BreedResult.kt
@@ -3,7 +3,7 @@ package co.touchlab.brownfieldsdk.response
 import kotlinx.serialization.Serializable
 
 @Serializable
-internal data class BreedResult(
+data class BreedResult(
     val message: Map<String, List<String>>,
     var status: String
 )
