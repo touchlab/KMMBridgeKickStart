d6e81f6eba0d2ba31c10ff2ae52b87191e6f02b7
Julia Jakubcova <j.jakubcova@touchlab.co>
2022-10-19 13:40:08 +0200

    Remove ktlint.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,33 +7,12 @@ plugins {
     id("co.touchlab.faktory.kmmbridge") version libs.versions.kmmBridge.get() apply false
     id("com.squareup.sqldelight") version libs.versions.sqlDelight.get() apply false
     id("maven-publish")
-    alias(libs.plugins.ktlint)
 }
 
 allprojects {
     repositories {
         google()
         mavenCentral()
-    }
-}
-
-subprojects {
-    // TODO libs doesn't resolve if we do this
-    // apply(plugin = libs.plugins.ktlint.get().pluginId)
-    apply(plugin = "org.jlleitschuh.gradle.ktlint")
-
-    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
-        enableExperimentalRules.set(true)
-        verbose.set(true)
-        filter {
-            exclude { it.file.path.contains("build/") }
-        }
-    }
-
-    afterEvaluate {
-        tasks.named("check").configure {
-            dependsOn(tasks.getByName("ktlintCheck"))
-        }
     }
 }
 
