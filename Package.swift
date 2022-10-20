cbec445fb91af57ec7d6c1df71451508f24bbed7
Kevin Galligan <kgalligan@gmail.com>
2022-10-19 20:50:22 -0400

    KMM SPM package release for 0.2.3

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://maven.pkg.github.com/touchlab/Brownfield-SDK/co/touchlab/brownfield-sdk/allshared-kmmbridge/0.2.0/allshared-kmmbridge-0.2.0.zip"
-let remoteKotlinChecksum = "460affead05546707522af90bdc52c4c53ba2d07d199d94d263240f05d118478"
+let remoteKotlinUrl = "https://maven.pkg.github.com/touchlab/Brownfield-SDK/co/touchlab/brownfield-sdk/allshared-kmmbridge/0.2.3/allshared-kmmbridge-0.2.3.zip"
+let remoteKotlinChecksum = "934300f624f4ccb619a041b4fb3890f6d2621e68f09398f3e01fca6a5b9e6b40"
 let packageName = "allshared"
 
 let package = Package(
