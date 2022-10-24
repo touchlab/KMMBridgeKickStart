7c5d9fcbfec5d8f12db7ff27ef98d67c1b494017
Julia Jakubcova <j.jakubcova@touchlab.co>
2022-10-24 15:59:39 +0200

    [WIP] Testing code.

--- allshared/build.gradle.kts
+++ allshared/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     kotlin("multiplatform")
@@ -7,9 +9,21 @@ plugins {
 }
 
 kotlin {
-    ios()
+    ios {
+//        binaries {
+//            this.framework {
+//                this.linkerOpts += "-lsqlite3"
+//            }
+//        }
+    }
     // Note: iosSimulatorArm64 target requires that all dependencies have M1 support
-    iosSimulatorArm64()
+    iosSimulatorArm64 {
+//        binaries {
+//            this.framework {
+//                this.linkerOpts += "-lsqlite3"
+//            }
+//        }
+    }
 
     sourceSets {
         val commonMain by getting {
@@ -33,17 +47,58 @@ kotlin {
         }
     }
 
+//    targets.withType<KotlinNativeTarget> {
+//        this.compilations.forEach {
+//            it.kotlinOptions.freeCompilerArgs += listOf("-linker-options", "-lsqlite3")
+//        }
+//    }
+
     cocoapods {
         summary = "KMMBridgeKickStart sample"
         homepage = "https://www.touchlab.co"
-        ios.deploymentTarget = "13.5"
+        ios.deploymentTarget = "14.1"
         extraSpecAttributes["libraries"] = "'c++', 'sqlite3'"
 
         framework {
             export(project(":analytics"))
             isStatic = true
+//            embedBitcode = org.jetbrains.kotlin.gradle.plugin.mpp.BitcodeEmbeddingMode.DISABLE
         }
     }
+
+//    val list = listOf(
+//        "_sqlite3_bind_blob",
+//        "_sqlite3_bind_double",
+//        "_sqlite3_bind_int64",
+//        "_sqlite3_bind_null",
+//        "_sqlite3_bind_parameter_index",
+//        "_sqlite3_bind_text",
+//        "_sqlite3_busy_timeout",
+//        "_sqlite3_changes",
+//        "_sqlite3_clear_bindings",
+//        "_sqlite3_close",
+//        "_sqlite3_close_v2",
+//        "_sqlite3_column_blob",
+//        "_sqlite3_column_bytes",
+//        "_sqlite3_column_count",
+//        "_sqlite3_column_double",
+//        "_sqlite3_column_int64",
+//        "_sqlite3_column_name",
+//        "_sqlite3_column_text",
+//        "_sqlite3_column_type",
+//        "_sqlite3_db_config",
+//        "_sqlite3_db_readonly",
+//        "_sqlite3_errmsg",
+//        "_sqlite3_finalize",
+//        "_sqlite3_last_insert_rowid",
+//        "_sqlite3_open_v2",
+//        "_sqlite3_prepare16_v2",
+//        "_sqlite3_reset",
+//        "_sqlite3_step",
+//    )
+//    crashLinkerConfig(
+//        list.joinToString(" ") { "-U $it" }
+//    )
 }
 
 addGithubPackagesRepository()
@@ -53,4 +108,21 @@ kmmbridge {
     githubReleaseVersions()
     spm()
 //    cocoapods("git@github.com:touchlab/PodSpecs.git")
+}
+
+fun org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension.crashLinkerConfig(linkerOpts: String) {
+    targets.withType(KotlinNativeTarget::class.java)
+        .map { target ->
+            val mainCompilation = target.compilations.getByName("main")
+            val dynamicFrameworks =
+                target.binaries.filterIsInstance<org.jetbrains.kotlin.gradle.plugin.mpp.Framework>()
+                    .filter { framework -> !framework.isStatic }
+
+            Pair(mainCompilation, dynamicFrameworks)
+        }
+        .forEach { pair ->
+            if (!pair.second.isEmpty()) {
+                pair.first.kotlinOptions.freeCompilerArgs += listOf("-linker-options", linkerOpts)
+            }
+        }
 }
